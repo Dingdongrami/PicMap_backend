5108d7040e28f7129d5cf37b8ade8f1758a3df9b
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-10-15 18:52:57 +0900

    chore: project 생성

---
+++ src/main/java/com/dingdong/picmap/PicMapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dingdong.picmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicMapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicMapBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dingdong/picmap/PicMapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingdong.picmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicMapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
