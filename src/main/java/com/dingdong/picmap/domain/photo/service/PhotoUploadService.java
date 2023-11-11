684a4eb5495c25e034ec345f8efbc03f79446267
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-11-12 04:01:28 +0900

    feat: metadata 추출

--- src/main/java/com/dingdong/picmap/domain/photo/service/PhotoUploadService.java
+++ src/main/java/com/dingdong/picmap/domain/photo/service/PhotoUploadService.java
@@ -3,14 +3,24 @@ package com.dingdong.picmap.domain.photo.service;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.dingdong.picmap.domain.photo.entity.Photo;
 import com.dingdong.picmap.domain.photo.repository.PhotoUploadRepository;
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Directory;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
+import com.drew.metadata.exif.GpsDirectory;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Map;
+
 
 @Slf4j
 @Service
@@ -22,6 +32,7 @@ public class PhotoUploadService {
     @Autowired
     private S3Uploader s3Uploader;
 
+    @SneakyThrows
     @Transactional
     public Long uploadPhoto(MultipartFile image, Photo requestPhoto) throws IOException {
         log.info("service - uploadPhoto ; image: {}", image);
@@ -30,12 +41,33 @@ public class PhotoUploadService {
             throw new IllegalArgumentException("image is null");
         }
 
-        String storeFileName = s3Uploader.upload(image, "images");
-        requestPhoto.setFilePath(storeFileName);
-        // s3 upload 한 후에 해당 사진의 metadata 에서 longitude, latitude 추출
+        File file = s3Uploader.convert(image)
+                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
+        String storeFileName = s3Uploader.upload(file, "images");
 
+        Map<String, Directory> readMetadata = s3Uploader.readMetadata(file);
+        double latitude = 0;
+        double longitude = 0;
+        // 임시 날짜
+        Date shootingDate = new Date();
+
+        if (readMetadata != null) {
+            GpsDirectory gps = (GpsDirectory) readMetadata.get("gps");
+            ExifSubIFDDirectory exif = (ExifSubIFDDirectory) readMetadata.get("exif");
+
+            latitude = gps.getGeoLocation().getLatitude();
+            longitude = gps.getGeoLocation().getLongitude();
+            shootingDate = exif.getDateOriginal();
+        }
+
+        log.info("readMetadata finished");
+        requestPhoto.setFilePath(storeFileName);
+        requestPhoto.setShootingDate(shootingDate);
+        requestPhoto.setLatitude(latitude);
+        requestPhoto.setLongitude(longitude);
 
         Photo savedPhoto = photoUploadRepository.save(requestPhoto);
+        s3Uploader.readMetadata(file);
         return savedPhoto.getId();
     }
 
