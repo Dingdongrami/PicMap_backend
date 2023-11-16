9faa1f3d9cef0f4a82e9abcd5353a6d118336313
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-11-17 02:41:50 +0900

    fix: SecurityConfig filter 에 securityUtils 추가

--- src/main/java/com/dingdong/picmap/config/SecurityConfig.java
+++ src/main/java/com/dingdong/picmap/config/SecurityConfig.java
@@ -2,14 +2,20 @@ package com.dingdong.picmap.config;
 
 import com.dingdong.picmap.config.jwt.JwtAuthenticationFilter;
 import com.dingdong.picmap.config.jwt.JwtTokenProvider;
+import com.dingdong.picmap.config.util.SecurityUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -20,6 +26,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final SecurityUtils securityUtils;
 
     @Bean
     public PasswordEncoder encoder() {
@@ -39,7 +46,7 @@ public class SecurityConfig {
                 .antMatchers("/api/user/**").permitAll()
 //                .anyRequest().authenticated()
                 .and()
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, securityUtils), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
