ef5456a64996b317256d9d65c77f459a8c44d496
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-11-23 00:18:16 +0900

    fix: User 에 UserDetails 추가
    - login 설정 중
    - sign up 기능 추가

--- src/main/java/com/dingdong/picmap/domain/user/dto/SignupRequestDto.java
+++ src/main/java/com/dingdong/picmap/domain/user/dto/SignupRequestDto.java
@@ -1,12 +1,11 @@
 package com.dingdong.picmap.domain.user.dto;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-public class LoginRequest {
+public class SignupRequestDto {
 
     private String email;
     private String password;
+    private String nickname;
 }
