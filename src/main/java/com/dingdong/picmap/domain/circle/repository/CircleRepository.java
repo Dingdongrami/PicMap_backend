23522c9847c00998eda13ac048f8d2f05d3fe5ec
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-11-22 14:13:58 +0900

    feat: Circle 조회 구현
    - userId로 조회
    - status=PUBLIC 인 경우

--- src/main/java/com/dingdong/picmap/domain/circle/repository/CircleRepository.java
+++ src/main/java/com/dingdong/picmap/domain/circle/repository/CircleRepository.java
@@ -3,5 +3,9 @@ package com.dingdong.picmap.domain.circle.repository;
 import com.dingdong.picmap.domain.circle.entity.Circle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CircleRepository extends JpaRepository<Circle, Long> {
+
+    List<Circle> findByStatus(String status);
 }
