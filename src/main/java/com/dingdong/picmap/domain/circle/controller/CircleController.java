6705fca32fa72efd87efaffe8d2104445127bba8
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-11-22 14:13:58 +0900

    feat: Circle 조회 구현
    - userId로 조회
    - status=PUBLIC 인 경우

--- src/main/java/com/dingdong/picmap/domain/circle/controller/CircleController.java
+++ src/main/java/com/dingdong/picmap/domain/circle/controller/CircleController.java
@@ -3,10 +3,13 @@ package com.dingdong.picmap.domain.circle.controller;
 import com.dingdong.picmap.domain.circle.dto.CircleCreateRequestDto;
 import com.dingdong.picmap.domain.circle.dto.CircleResponseDto;
 import com.dingdong.picmap.domain.circle.service.CircleCreateService;
+import com.dingdong.picmap.domain.circle.service.CircleService;
 import com.dingdong.picmap.domain.global.BaseTimeEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class CircleController extends BaseTimeEntity {
 
     private final CircleCreateService circleCreateService;
+    private final CircleService circleService;
 
     // 써클 생성
     @PostMapping("/add-circle")
@@ -21,4 +25,15 @@ public class CircleController extends BaseTimeEntity {
         return ResponseEntity.ok(circleCreateService.createCircle(request));
     }
 
+    // 해당 user 가 가입된 써클 리스트 조회
+    @GetMapping("/list/{userId}")
+    public ResponseEntity<List<CircleResponseDto>> getCircles(@PathVariable Long userId) {
+        return ResponseEntity.ok(circleService.getCirclesByUser(userId));
+    }
+
+    // public 한 써클 리스트 조회
+    @GetMapping("/public")
+    public ResponseEntity<List<CircleResponseDto>> getPublicCircles() {
+        return ResponseEntity.ok(circleService.getPublicCircles());
+    }
 }
---
+++ src/main/java/com/dingdong/picmap/domain/circle/service/CircleService.java
@@ -0,0 +1,43 @@
+package com.dingdong.picmap.domain.circle.service;
+
+import com.dingdong.picmap.domain.circle.dto.CircleResponseDto;
+import com.dingdong.picmap.domain.circle.entity.Circle;
+import com.dingdong.picmap.domain.circle.repository.CircleRepository;
+import com.dingdong.picmap.domain.circle.repository.CircleUserRepository;
+import com.dingdong.picmap.domain.user.entity.User;
+import com.dingdong.picmap.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CircleService {
+
+    private final CircleRepository circleRepository;
+    private final CircleUserRepository circleUserRepository;
+    private final UserRepository userRepository;
+
+    public List<CircleResponseDto> getCirclesByUser(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+        List<Circle> circles = circleUserRepository.findCirclesByUserId(user);
+        return circles.stream()
+                .map(circle -> CircleResponseDto.builder()
+                        .id(circle.getId())
+                        .name(circle.getName())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    public List<CircleResponseDto> getPublicCircles() {
+        List<Circle> circles = circleRepository.findByStatus("PUBLIC");
+        return circles.stream()
+                .map(circle -> CircleResponseDto.builder()
+                        .id(circle.getId())
+                        .name(circle.getName())
+                        .build())
+                .collect(Collectors.toList());
+    }
+}
