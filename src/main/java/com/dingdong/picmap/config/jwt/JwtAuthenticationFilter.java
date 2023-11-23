feeb3422ff2ead5cea5314eb7eda114ae96443f6
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-11-24 02:55:21 +0900

    refactor: 불필요 코드 정리

--- src/main/java/com/dingdong/picmap/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/dingdong/picmap/config/jwt/JwtAuthenticationFilter.java
@@ -3,16 +3,10 @@ package com.dingdong.picmap.config.jwt;
 import com.dingdong.picmap.config.util.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.util.StringUtils;
-import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
