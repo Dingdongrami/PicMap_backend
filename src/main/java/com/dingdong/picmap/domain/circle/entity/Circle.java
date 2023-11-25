fa90c61d2310a721b45301940ba3f256b28e6bfc
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-11-26 04:34:35 +0900

    fix: Circle thumbnail 필드 추가

--- src/main/java/com/dingdong/picmap/domain/circle/entity/Circle.java
+++ src/main/java/com/dingdong/picmap/domain/circle/entity/Circle.java
@@ -19,11 +19,13 @@ public class Circle {
     private String name;
     private String description;
     private String status;  // PUBLIC, PRIVATE
+    private String thumbnail;
 
     @Builder
-    public Circle(String name, String description, String status) {
+    public Circle(String name, String description, String status, String thumbnail) {
         this.name = name;
         this.description = description;
         this.status = status;
+        this.thumbnail = thumbnail;
     }
 }
