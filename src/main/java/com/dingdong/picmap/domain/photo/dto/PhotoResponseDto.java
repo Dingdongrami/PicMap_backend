8ebf437f5dd3c87c3a66ac77f20af09c16ba09f6
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-12-03 12:53:34 +0900

    fix: PhotoResponse 수정

--- src/main/java/com/dingdong/picmap/domain/photo/dto/PhotoResponseDto.java
+++ src/main/java/com/dingdong/picmap/domain/photo/dto/PhotoResponseDto.java
@@ -13,16 +13,28 @@ import java.util.stream.Collectors;
 public class PhotoResponseDto {
     private Long id;
     private String filePath;
+    private Integer likeCount;
+    private Integer commentCount;
+    private Double latitude;
+    private Double longitude;
 
     public PhotoResponseDto(Photo photo) {
         this.id = photo.getId();
         this.filePath = photo.getFilePath();
+        this.likeCount = photo.getLikes().size();
+        this.commentCount = photo.getComments().size();
+        this.latitude = photo.getLatitude();
+        this.longitude = photo.getLongitude();
     }
 
     public static PhotoResponseDto of(Photo photo) {
     	return PhotoResponseDto.builder()
     			.id(photo.getId())
     			.filePath(photo.getFilePath())
+          .likeCount(photo.getLikes().size())
+          .commentCount(photo.getComments().size())
+          .latitude(photo.getLatitude())
+          .longitude(photo.getLongitude())
     			.build();
     }
 
