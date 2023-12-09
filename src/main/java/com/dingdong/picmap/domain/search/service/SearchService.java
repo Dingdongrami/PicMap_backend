2bc396e75619bd9f10cda74a8a8876648f97e885
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-12-09 21:04:44 +0900

    fix: 장소 검색 로직 수정
    - 유저가 가입된 써클에서 검색하도록 수정

--- src/main/java/com/dingdong/picmap/domain/search/service/SearchService.java
+++ src/main/java/com/dingdong/picmap/domain/search/service/SearchService.java
@@ -2,11 +2,13 @@ package com.dingdong.picmap.domain.search.service;
 
 import com.dingdong.picmap.domain.circle.entity.Circle;
 import com.dingdong.picmap.domain.circle.repository.CircleRepository;
+import com.dingdong.picmap.domain.circle.repository.CircleUserRepository;
 import com.dingdong.picmap.domain.photo.dto.PhotoResponseDto;
 import com.dingdong.picmap.domain.photo.entity.Photo;
-import com.dingdong.picmap.domain.photo.repository.PhotoRepository;
-import com.dingdong.picmap.domain.photo.service.PhotoService;
+import com.dingdong.picmap.domain.search.dto.SearchRequestDto;
 import com.dingdong.picmap.domain.sharedAlbum.repository.CircleSharedAlbumRepository;
+import com.dingdong.picmap.domain.user.entity.User;
+import com.dingdong.picmap.domain.user.repository.UserRepository;
 import com.google.maps.model.LatLng;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,18 +26,25 @@ public class SearchService {
     private final GeoService geoService;
     private final CircleRepository circleRepository;
     private final CircleSharedAlbumRepository circleSharedAlbumRepository;
+    private final UserRepository userRepository;
+    private final CircleUserRepository circleUserRepository;
 
     public Map<String, LatLng> searchLocationWithBounds(String address) {
         return geoService.getLocationWithBounds(address);
     }
 
-    public List<PhotoResponseDto> searchPhoto(String address) {
-        Map<String, LatLng> locationWithBounds = geoService.getLocationWithBounds(address);
+    public List<PhotoResponseDto> searchPhoto(SearchRequestDto requestDto) {
+        Map<String, LatLng> locationWithBounds = geoService.getLocationWithBounds(requestDto.getAddress());
         LatLng northEast = locationWithBounds.get("northEast");
         LatLng southWest = locationWithBounds.get("southWest");
 
-        List<Circle> publicCircleList = circleRepository.findByStatus("PUBLIC");
-        List<Circle> governmentCircleList = circleRepository.findByStatus("GOVERNMENT");
+        User user = userRepository.findById(requestDto.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+
+        List<Circle> publicCircleList = circleUserRepository.findCirclesByUser(user)
+                .stream()
+                .filter(circle -> circle.getStatus().equals("PUBLIC") || circle.getStatus().equals("GOVERNMENT"))
+                .collect(Collectors.toList());
 
         List<Photo> photoList = publicCircleList.stream()
                 .map(circleSharedAlbumRepository::findAllByCircle)
@@ -44,14 +53,6 @@ public class SearchService {
                 .filter(photo -> photo.getLatitude() <= northEast.lat && photo.getLongitude() >= southWest.lng
                         && photo.getLatitude() >= southWest.lat && photo.getLongitude() <= northEast.lng)
                 .collect(Collectors.toList());
-
-        governmentCircleList.stream()
-                .map(circleSharedAlbumRepository::findAllByCircle)
-                .flatMap(List::stream)
-                .filter(photo -> photo.getLatitude() != null && photo.getLongitude() != null)
-                .filter(photo -> photo.getLatitude() <= northEast.lat && photo.getLongitude() >= southWest.lng
-                        && photo.getLatitude() >= southWest.lat && photo.getLongitude() <= northEast.lng)
-                .forEach(photoList::add);
 
         return photoList.stream()
                 .map(PhotoResponseDto::of)
